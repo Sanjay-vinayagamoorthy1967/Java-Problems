a0d75b2d87c407b2b8ffe056c5cc3aea1a2c9c75
Sanjay Vinayagamoorthy <sanjay.v2382k05@gmail.com>
2026-02-16 00:48:01 +0530

    j

--- Basics/Main5.java
+++ Basics/Main5.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class Main5 {
         public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int totalBill = sc.nextInt();
-        int service   = sc.nextInt();
-        int gst = sc.nextInt();
-        int numberOfPeople = sc.nextInt();
+        float totalBill = sc.nextInt();
+        float service   = sc.nextInt();
+        float gst = sc.nextInt();
+        float numberOfPeople = sc.nextInt();
         float percentage = (totalBill*service)/100;
-        float per = (totalBill*gst)/100;
-        float total = totalBill+percentage+per;
+        float bill = totalBill+percentage;
+        float per = (bill*gst)/100;
+        float total = bill+per;
         float tot = total/numberOfPeople;
         System.out.println(tot);
         
