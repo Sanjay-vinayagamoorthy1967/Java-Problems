cad8914bcfd5a6e0bab64511f7b4f92c010fb036
Sanjay Vinayagamoorthy <sanjay.v2382k05@gmail.com>
2026-02-17 20:43:39 +0530

    Add files via upload

---
+++ Basics/Main22.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main22 {
+    public static void main(String[] args) {
+         
+        Scanner sc = new Scanner(System.in);
+        int copies = sc.nextInt();
+        int cost = sc.nextInt();
+        int editing = sc.nextInt();
+        int design = sc.nextInt();
+        int commission = sc.nextInt();
+        int marketing = sc.nextInt();
+       
+        int Profit =(copies * cost) + editing + design - commission - marketing;
+        
+        System.out.println(Profit);  
+    } 
+}
