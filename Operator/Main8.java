0258e8251401e0d7e2c0b3c4694b5692722d6c11
Sanjay Vinayagamoorthy <sanjay.v2382k05@gmail.com>
2026-02-18 21:50:51 +0530

    Add files via upload

---
+++ Operator/Main8.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        int idproof = sc.nextInt();
+        if(age >= 18 && idproof == 1){
+            System.out.println("Allowed");
+        }else{
+            System.out.println("Denied");
+        }
+    }
+}
