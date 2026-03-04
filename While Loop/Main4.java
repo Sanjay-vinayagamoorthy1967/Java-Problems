0902e9893ef8d78695c6c6cb718db2b15c541fdb
Sanjay Vinayagamoorthy <sanjay.v2382k05@gmail.com>
2026-03-04 22:26:39 +0530

    Add files via upload

---
+++ While Loop/Main4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 1;
+        int fact =1;
+        while(i<=n){
+            fact = fact*i;
+            i++;
+        }
+        System.out.println("Factorial of "+n+" = "+fact);
+        sc.close();
+    }
+}
