27d0cee8c3973ddf82b3fd8595882b1f76219d2c
Sanjay Vinayagamoorthy <sanjay.v2382k05@gmail.com>
2026-03-05 21:41:43 +0530

    Add files via upload

---
+++ While Loop/Main5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int temp = n;
+        int rev =0;
+        while(n!=0){
+            int dig = n%10;
+            rev = rev*10+dig;
+            n /= 10;
+        }
+        System.out.println("Original: "+temp);
+        System.out.println("Reversed: "+rev);
+        sc.close();
+    }
+}
