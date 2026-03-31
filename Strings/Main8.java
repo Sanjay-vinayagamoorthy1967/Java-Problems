7426155d34e997009d9e33e796d995f916a6483f
Sanjay Vinayagamoorthy <sanjay.v2382k05@gmail.com>
2026-03-31 21:00:42 +0530

    P
    Fixed syntax error in the Scanner input line and corrected HTML entity representations.

--- Strings/Main8.java
+++ Strings/Main8.java
@@ -4,7 +4,7 @@ public class Main8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        sc.nextLine();
+        sc.nextLine():
 
         for (int i = 0; i < n; i++) {
             String str = sc.next().toLowerCase();
