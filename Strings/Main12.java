695352c6947cd4b4121133e14553bc08261fd20b
Sanjay Vinayagamoorthy <sanjay.v2382k05@gmail.com>
2026-04-02 21:29:48 +0530

    Add files via upload

---
+++ Strings/Main12.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        for(int i=0;i<n;i++){
+            String str = sc.nextLine();
+            System.out.println(i+1 +". ["+str+"]");
+        }
+        sc.close();
+    }   
+}
