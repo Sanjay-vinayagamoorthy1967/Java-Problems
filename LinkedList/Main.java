bb3de6172e1981a9fbb834a1b5014a9d09dc8c42
Sanjay Vinayagamoorthy <sanjay.v2382k05@gmail.com>
2026-09-01 23:07:46 +0530

    p

--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);  
         LinkedList<Integer> arr = new LinkedList<Integer>();
         int n = sc.nextInt();
         for(int i=0;i<n;i++){
